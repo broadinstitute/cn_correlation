4f6af2205fad1252b07a74190da2baddf592f9c9
stevenschumacher <schum@broadinstitute.org>
2020-05-07 08:57:00 -0400

    updated example code to use GISTIC inputs stored in repo instead of hardwired to /xchip/gistic/...

--- example/ng_pancan_corrperm_hl_recap.m
+++ example/ng_pancan_corrperm_hl_recap.m
@@ -1,53 +1,56 @@
-% validation code
-% recapitulate Travis's correlation analysis for version control factoring 
-% (adapted from Nature Genetics submission main correlation script 
-% /xchip/gistic/Travis/Correlations/LSF2/0816/NG_ll_wrap.m)
-%
 % calls corrperm_prep to prepare data for 'high level' permutations
 
 clear
-dbstop if error
+%dbstop if error
 set_verbose_level(40)
 
-% local working directory
-reference_dir = [pwd,'/work_dir/'];
+% local working directories
+%ref_dir = [pwd,'/hl_work/'];     % where permutation inputs are stored
+ref_dir = '/xchip/beroukhimlab/gistic/corrperm/hl_work/';
+perm_dir = [ref_dir,'/hl_permout/']; % where permutation outputs are stored
+anal_dir = [ref_dir,'/hl_analysis1/'];
 
-% data used in the pancan NG paper
-dd = '/xchip/gistic/tcga_projects/pancancer/data_analysis_two/';
-gis_dir = [dd,'gistic_analyses/blen_cutoff_series_130814/results/blen0.5_narmp/'];
-load([gis_dir,'peak_regs.blen0.5_narmp.mat']); %regs file
+% input files
+gistic_peaks_file = [pwd,'/peak_regs.blen0.5_narmp.mat']; % peak results from GISTIC
+gistic_data_file = [pwd,'/D.cap1.5.blen0.5_narmp.mat'];   % copy-number data w/ziggurat 
+peak_name_files = {[pwd,'/amp_peak_info.txt'],[pwd,'/del_peak_info.txt']}; % peak friendly names
+subtype_info_file = [pwd,'/pancan12.sample_info_w_subtypes.130815.txt'];   % subtype information
 
-% add peak name to regs
+% "bare load" matlab peak file into variable 'regs'
+load(gistic_peaks_file);
+
+% add manually edited peak name to regs (for results)
 amp_del = {'amp','del'};
 for k=1:2
-    peak_info = read_R_table([dd,'current_dataset/',amp_del{k},'_peak_info.txt']);
+    peak_info = read_R_table(peak_name_files{k});
     [~,order] = sort([regs{k}.resid_qv]);
     for p=1:length(regs{k})
         regs{k}(order(p)).name = peak_info(p).name;
     end
 end
 
-D = load_D([gis_dir,'D.cap1.5.blen0.5_narmp.mat']); %D_file
-SIF = '/xchip/gistic/tcga_projects/pancancer/data_analysis_two/current_dataset/archive/pancan12.sample_info_w_subtypes.130815.txt';
-sif = read_R_table(SIF);    
+%% Fix sample information in D.sis
+% this analysis used subtypes to further qualify lineage / restrict permutations
 
-sif_names = {sif.tcga_id};
+%% type/subtype processing
+sif = read_R_table(subtype_info_file);    
 
-[~,i1,i2] = intersect(sif_names,D.sdesc);
-
-D = reorder_D_cols(D,i2);
-
-%remove "normal"
+% blank "Normal" subtypes
 temp = strmatch('Normal',{sif.subtype});
 for i = 1:length(temp)
-    sif(temp(i)).subtype = [];
+    sif(temp(i)).subtype = '';
 end
 
-%replace "READ" with COAD 
+% treat rectal as colon adenocarcinoma: replace "READ" with COAD 
 temp = strmatch('READ',{sif.disease});
 for i = 1:length(temp)
     sif(temp(i)).disease = 'COAD';
 end
+
+% load copy number data and update sample information
+D = load_D(gistic_data_file);
+[~,i1,i2] = intersect({sif.tcga_id},D.sdesc);
+D = reorder_D_cols(D,i2);
 D.sis = sif(i1);
 
 % further qualify disease with subtype
@@ -55,7 +58,7 @@ for i = 1:length(D.sdesc)
     D.sis(i).disease = [D.sis(i).disease,'_',D.sis(i).subtype];
 end
 
-% set the options for high-level analysis
+% set the options for preparing high-level analysis
 options = struct;
 options.hilevel = true;             % false for low-level
 options.event_thresh = 0.95;        % 0 for high-level
@@ -68,7 +71,7 @@ options.permclass_sisfield = 'disease';
 
 
 % prepare data for permutations
-[D,margs_sort,new_samples] = corrperm_prep(D,regs,reference_dir,options);
+[D,margs_sort,new_samples] = corrperm_prep(D,regs,ref_dir,options);
 
 %% set parameters for low-level tempered annealing
 temparams = struct;
@@ -100,7 +103,21 @@ keyboard
 
 %% LSF runs
 
-%! TODO!!!
+%!corrperm_lsf_submission('corrperm_ampdel_tempering_module',ref_dir,perm_dir,500,100,temparams);
+corrperm_uger_submission('corrperm_ampdel_tempering_module',ref_dir,perm_dir,500,100,temparams);
 
 %% wait for all those permutations to complete!
 keyboard
+
+% analyze pair correlations
+options = struct;
+options.sig_thresh = 0.95;
+options.power_thresh = 0.1;
+options.tail = 'both';
+options.split_eq = true;
+options.ext = 'hl_pair';
+options.lineage_out = unique({D.sis.disease});
+options.perm_file_mask = 'idx_cell.chunk.*.mat';
+options.pcount = 0;
+
+corrperm_analyze_pairs(ref_dir,perm_dir,anal_dir,options);
--- example/ng_pancan_corrperm_ll_recap.m
+++ example/ng_pancan_corrperm_ll_recap.m
@@ -1,53 +1,60 @@
 % validation code
-% recapitulate Travis's correlation analysis for version control factoring 
+% recapitulate Travis's correlation analysis for version control factoring
 % (adapted from Nature Genetics submission main correlation script 
 % /xchip/gistic/Travis/Correlations/LSF2/0816/NG_ll_wrap.m)
 %
 % calls corperm_prep to prepare data for 'low level' permutations
 
 clear
-dbstop if error
+%dbstop if error
 set_verbose_level(40)
 
-% local working directory
-reference_dir = [pwd,'/work_dir/'];
+% local working directories
+ref_dir = [pwd,'/ll_work/'];     % where permutation inputs are stored
+perm_dir = [ref_dir,'/ll_permout/']; % where permutation outputs are stored
 
-% data used in the pancan NG paper
-dd = '/xchip/gistic/tcga_projects/pancancer/data_analysis_two/';
-gis_dir = [dd,'gistic_analyses/blen_cutoff_series_130814/results/blen0.5_narmp/'];
-load([gis_dir,'peak_regs.blen0.5_narmp.mat']); %regs file
+% input files
+gistic_peaks_file = [pwd,'/peak_regs.blen0.5_narmp.mat']; % peak results from GISTIC
+gistic_data_file = [pwd,'/D.cap1.5.blen0.5_narmp.mat'];   % copy-number data w/ziggurat 
+peak_name_files = {[pwd,'/amp_peak_info.txt'],[pwd,'/del_peak_info.txt']}; % peak friendly names
+subtype_info_file = [pwd,'/pancan12.sample_info_w_subtypes.130815.txt'];   % subtype information
 
-% add peak name to regs
+% "bare load" matlab peak file into variable 'regs'
+load(gistic_peaks_file);
+
+% add manually edited peak name to regs (for results)
 amp_del = {'amp','del'};
 for k=1:2
-    peak_info = read_R_table([dd,'current_dataset/',amp_del{k},'_peak_info.txt']);
+    peak_info = read_R_table(peak_name_files{k});
     [~,order] = sort([regs{k}.resid_qv]);
     for p=1:length(regs{k})
         regs{k}(order(p)).name = peak_info(p).name;
     end
 end
 
-D = load_D([gis_dir,'D.cap1.5.blen0.5_narmp.mat']); %D_file
-SIF = '/xchip/gistic/tcga_projects/pancancer/data_analysis_two/current_dataset/archive/pancan12.sample_info_w_subtypes.130815.txt';
-sif = read_R_table(SIF);    
 
-sif_names = {sif.tcga_id};
+%% Fix sample information in D.sis
+% this analysis used subtypes to further qualify lineage / restrict permutations
 
-[~,i1,i2] = intersect(sif_names,D.sdesc);
+%% type/subtype processing
+sif = read_R_table(subtype_info_file);    
 
-D = reorder_D_cols(D,i2);
-
-%remove "normal"
+% blank "Normal" subtypes
 temp = strmatch('Normal',{sif.subtype});
 for i = 1:length(temp)
-    sif(temp(i)).subtype = [];
+    sif(temp(i)).subtype = '';
 end
 
-%replace "READ" with COAD 
+% treat rectal as colon adenocarcinoma: replace "READ" with COAD 
 temp = strmatch('READ',{sif.disease});
 for i = 1:length(temp)
     sif(temp(i)).disease = 'COAD';
 end
+
+% load copy number data and update sample information
+D = load_D(gistic_data_file);
+[~,i1,i2] = intersect({sif.tcga_id},D.sdesc);
+D = reorder_D_cols(D,i2);
 D.sis = sif(i1);
 
 % further qualify disease with subtype
@@ -66,13 +73,13 @@ options.broad_len_cutoff = 0.50;    % 0.55 for high-level
 options.max_disruption = [Inf,Inf]; % [160000,600000] for high level
 options.permclass_sisfield = 'disease';
 
-% prepare data for permutations
-[D,margs_sort,new_samples] = corrperm_prep(D,regs,reference_dir,options);
+%% prepare data for permutations
+[D,margs_sort,new_samples] = corrperm_prep(D,regs,ref_dir,options);
 
 %% set parameters for low-level tempered annealing
 temparams = struct;
 % misc
-temparams.algorithm = 'basic_tempered_annealing';
+temparams.algorithm = 'basic_tempered_annealing'; % (for validation, does not set algorithm)
 temparams.stat_interval = 1000;
 temparams.class_subiters = 100;
 temparams.ramp_steps = 10;
@@ -106,3 +113,17 @@ corrperm_lsf_submission('corrperm_ampdel_tempering_module',ref_dir,perm_dir,500,
 
 %% wait for all those permutations to complete!
 keyboard
+
+% analyze pair correlations
+options = struct;
+options.sig_thresh = 0.95;
+options.power_thresh = 0.1;
+options.tail = 'both';
+options.split_eq = true;
+options.ext = 'll_pair';
+options.lineage_out = unique({D.sis.gcmtype});
+options.perm_file_mask = 'idx_cell.chunk.*.mat';
+options.pcount = 0;
+
+corrperm_analyze_pairs(ref_dir,perm_dir,ref_dir,options);
+%corrperm_analyze_features(features,ref_dir,perm_dir,ref_dir,options);
