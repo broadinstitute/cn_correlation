5c7fdd316fbf43893711f49668b3034ccc9f2956
stevenschumacher <schum@broadinstitute.org>
2020-06-11 18:42:33 -0400

    import/export of event maps now uses two tables, event.info and event.call.

--- export_emap.m
+++ export_emap.m
@@ -1,21 +1,46 @@
-function export_emap(E,fname)
-%EXPORT_EMAP write event map to a text file
+function export_emap(E,evif_fname,ecall_fname)
+%EXPORT_EMAP write event map as tab-delimited text files
 %
-%    export_emap(EMAP,FNAME)
+%    export_emap(EMAP,EVIF_FNAME,ECALL_FNAME)
 %
 % EMAP is an event mapping structure used to analyze permutations
-% FNAME is the path to the .txt file output
+% EVIF_FNAME is the name of the tab-delimited event information file to write
+% ECALL_FNAME is the name of the tab-delimited event call file to write
 %
     %% write event map text file
     tb = char(9); % tab character delimiter
     fad = {'f','a','d'}; % map integer to event type
     O12 = {'0','1','2'}; % map logical matrix to char
-    fid = fopen(fname,'w');
-    fprintf(fid,['name',tb,strjoin(E.event.name',tb),'\n']);
-    fprintf(fid,['type',tb,strjoin(fad(E.event.type+1),tb),'\n']);
-    fprintf(fid,['chrn',tb,strjoin(cellstr(num2str(E.event.chrn))',tb),'\n']);
+
+    % write event info file
+    other_fields = setdiff(fieldnames(E.event),{'name','type','chrn'});
+    other_cols = {};
+    for f = 1:length(other_fields)
+        event_data = E.event.(other_fields{f});
+        if isnumeric(event_data) && any(event_data-round(event_data))
+            other_cols{f} = {other_fields{f},E.event.(other_fields{f}),'%0.16g'};
+        else
+            other_cols{f} = {other_fields{f},E.event.(other_fields{f})};
+        end
+            
+    end
+
+    write_filtered_tabcols(evif_fname,[],{'event',E.event.name},...
+                           {'type',fad(E.event.type+1)},...
+                           {'chrn',E.event.chrn},other_cols{:});
+
+    % write event call file 
+    fid = fopen(ecall_fname,'w');
+    % construct 'lineage' output class column
+    lincol = repmat({''},size(E.sdesc));
+    for i = 1:length(E.pcx)
+        lincol(E.pcx{i}) = repmat(cellstr(E.pcname{i}),size(E.pcx{i}));
+    end
+    % write header
+    fprintf(fid,['sample',tb,'lineage',tb,strjoin(E.event.name',tb),'\n']);
+    % write body
     for i = 1:size(E.dat,2)
-        fprintf(fid,[E.sdesc{i},tb,strjoin(O12(E.dat(:,i)'+1),tb),'\n']);
+        fprintf(fid,[E.sdesc{i},tb,lincol{i},tb,strjoin(O12(E.dat(:,i)'+1),tb),'\n']);
     end
     fclose(fid);
 end
--- import_emap.m
+++ import_emap.m
@@ -1,10 +1,12 @@
-function E = import_emap(fname)
+function E = import_emap(evif_fname,ecall_fname)
 %IMPORT_EMAP read an event map from a text file
 %
-%     EMAP = import_emap(FNAME)
+%     EMAP = import_emap(EVIF_FNAME,EVCALL_FNAME)
 %
 % EMAP is an event mapping structure used to analyze permutations
-% FNAME is the path to the .txt file output
+% EVIF_FNAME is the tab-delimited event information file name to read
+% ECALL_FNAME is the tab-delimited event call file name read
+%
 %
 % Read a tab-delimited hash-commented text file containing an array
 % of logical sample X event occurrences into an event map. Event attributes
@@ -19,124 +21,180 @@ function E = import_emap(fname)
 % event attribute 'type' (1 or 'a' - amplification; 2 or 'd' - deletion; 
 % 0 or 'f' - feature).
 
-dlm = char(9); % default tab-delimited table
-nantext = 'NA'; %!!! disallow nans, eliminate
-nantext = ['^',nantext,'$'];
+    dlm = char(9); % tab-delimited tables
 
-%% read header for number of columns
-fid = fopen(fname);
+    nantext = 'NA'; %!!! disallow nans, eliminate
+    nantext = ['^',nantext,'$'];
 
-% scan over empty or R-style # comment lines
-skipline = true;
-while skipline
-    hdrline = fgets(fid);
-    skipline = isempty(hdrline) || hdrline(1) == '#';
-    %!    fprintf(hdrline); %!!!debug
-end
-
-hdr = regexp(hdrline,dlm,'split');
-Ncols = length(hdr);
-if Ncols == 0
-    error('event.map error: empty file');    
-end
-try_name = strtrim(hdr(1));
-if ~strcmp(try_name,'name')
-    error('event.map error: first row/column of event map must be ''name''.');
-end
-Nevents = Ncols-1;
-event = struct;
-event.name = strtrim(hdr(2:Ncols)');
-
-% process header line attributes
-no_chrn_yet = true;
-anames = cell(0); % header/attribute names so far
-while no_chrn_yet
-    anames = [anames,{try_name}];
-    hdrline = fgets(fid);
-    hdr = regexp(hdrline,dlm,'split');
-    if (length(hdr) ~= Ncols)
-        error('event.map error: uneven column count');
+    %% validate filename arguments
+    if ~exist('evif_fname','var') || isempty(evif_fname)
+        error('No event information file argument.');
     end
-    try_name = strtrim(hdr(1));
-    try_vals = strtrim(hdr(2:Ncols)');
-    if strcmp(try_name,'chrn')
-        event.chrn = chromosome2num(try_vals);
-        no_chrn_yet = false;
-    else 
-        if strcmp(try_name,'type')
-            try_nums = str2num(char(regexprep(regexprep(regexprep(try_vals,'f','0'),'a','1'),'d','2')));
-            if length(try_nums) == Nevents
-                event.type = try_nums;
-            else
-                error('event.map error: bad event ''type'' values.');
-            end
-            
-        else % unrecognized attribute
-             % clean up name to be valid field name
-            try_name = regexprep(strtrim(try_name),'\s+','_'); %!
-            try_name = regexprep(try_name,'[^A-Za-z0-9_]+','');
-            try_name = regexprep(try_name,'^([0-9_]+)','X$1');
-            % name de-duplication
-            if any(strcmp(try_name,anames))
-                n = 2;
-                try_n = try_name;
-                while any(strcmp(try_name,anames))
-                    try_name = [try_n,'_',num2str(n)];
-                    n = n+1;
-                end
-            end
-             % attempt to convert to number
-            try_nums = str2num(char(regexprep(tryvals,nantext,'NaN')));
-            if length(try_nums) == Nevents
-                % save as number
-                event.(try_name) = try_nums;
-            else
-                % keep as string
-                event.try_num = try_vals;
-            end
-            %    event(hdr(1)) = hdr(2:Ncols)
+    if ~exist('ecall_fname','var') || isempty(evif_fname)
+        error('No event call file argument.');
+    end
+    if ~exist(evif_fname,'file')
+        error(['Event information file ''',evif_fname,''' doesn''t exist.']);
+    end
+    if ~exist(ecall_fname,'file')
+        error(['Event call file ''',ecall_fname,''' doesn''t exist.']);
+    end
+
+    E = struct;
+    E.event = struct;
+
+    %%
+    %% process event information file
+    %%
+    etab = read_R_table(evif_fname,dlm);
+    % event column
+    assert(isfield(etab,'event'));
+    E.event.name = {etab.event}';
+    % type column
+    assert(isfield(etab,'type'));
+    [ok,idx] = ismember({etab.type},{'f','a','d'});
+    assert(all(ok)); % all recognized type
+    E.event.type = idx' - 1;
+    % chrn column
+    assert(isfield(etab,'chrn'));
+    if isnumeric( etab(1).chrn );
+        E.event.chrn = [etab.chrn]';
+    else
+        E.event.chrn = chromosome2num({etab.chrn}');
+    end
+
+    % field enumeration
+    other_fields = setdiff(fieldnames(etab),{'event','type','chrn'});
+    for f = 1:length(other_fields)
+        if isnumeric(etab(1).(other_fields{f}));
+            E.event.(other_fields{f}) = [etab.(other_fields{f})]';
+        else
+            E.event.(other_fields{f}) = {etab.(other_fields{f})}';
         end
     end
-end
 
-%% read in rest of file as cell array of columns
-
-% make textscan format string
-if hdrline(end) == char(10)
-    lf_char = '\n';%char(10);
-    pcunix_text = true;
-else
-    lf_char = '\r';%char(13);
-    pcunix_text = false;
-end
-
-% the first column is sample label, the rest numbers
-fmstr = ['%s',repmat(' %d',1,Nevents),lf_char];
-try
-    if pcunix_text
-        cols = textscan(fid,fmstr,'ReturnOnError',0,'Delimiter',dlm,'WhiteSpace','\r');
-    else
-        cols = textscan(fid,fmstr,'ReturnOnError',0,'Delimiter',dlm);
+    %%
+    %% process event call file
+    %%
+    fid = fopen(ecall_fname);
+    % scan over empty or R-style # comment lines
+    skipline = true;
+    while skipline
+        hdrline = fgets(fid);
+        skipline = isempty(hdrline) || hdrline(1) == '#';
+        %!    fprintf(hdrline); %!!!debug
     end
-    fclose(fid);
-catch me
-    fclose(fid);
-    error(['event map error: textscan error ''',me.message,'''']);
-end
+    % read header
+    hdr = regexp(hdrline,dlm,'split');
+    Ncols = length(hdr);
+    if Ncols == 0
+        error('event.map error: empty file');    
+    end
+    if Ncols < 2
+        error('event.call error: no events')
+    end
+    % sample column
+    try_name = strtrim(hdr(1));
+    if ~strcmp(try_name,'sample')
+        error('event.call error: first column of event map must be ''sample''.');
+    end
+    % optional lineage column
+    try_lineage = strtrim(hdr(2));
+    if strcmp(try_lineage,'lineage')
+        have_lineage = true;
+        Nevents = Ncols-2;
+        lin_fmt = ' %s';
+        called_events = strtrim(hdr(3:end));
+    else
+        have_lineage = false;
+        Nevents = Ncols-1;
+        lin_fmt = '';
+        called_events = strtrim(hdr(2:end));
+        if ~ismember(try_lineage,event.name)
+            error('event.call error: column 2 of event map must be either an event.info name or ''lineage''.');
+        end
+    end
 
-% test for short columns
-if any(diff(cellfun(@length,cols)))
-    error('event map error: missing event data');
-end
+    % discard event.info that does not match event.calls
+    efields = fieldnames(E.event);
+    matching_info = ismember(E.event.name,called_events);
+    if ~all(matching_info)
+        extra_info = sum(~matching_info);
+        warning([num2str(sum(~matching_info)),' events in event.info without event.calls discarded.']);
+        for f = 1:length(efields)
+            E.event.(efields{f}) = E.event.(efields{f})(matching_info);
+        end     
+    end
 
-E = struct;
-E.sdesc = cols{1};
-E.event = event;
+    % add event.calls without event.info as feature events
+    [isec,ia,ib] = intersect(called_events,E.event.name,'stable'); %!!! will error on early ML versions
+    if length(isec) < length(called_events)
+        unk_evx = find(~ismember(called_events,E.event.name))';
+        Nnewfeat = length(unk_evx);
+        warning([num2str(Nnewfeat),' event.calls present without event.info converted to features.']);
+        % create feature event info
+        E.event.name = [E.event.name;called_events(unk_evx)'];
+        %! E.event.chrn = [E.event.chrn;zeros(Nnewfeat,1)]; % chromosome "zero"
+        %! E.event.type = [E.event.chrn;zeros(Nnewfeat,1)]; % feature type
+        restfields = setdiff(efields,'name');
+        ib = [ib;unk_evx];
+        for f = 1:length(restfields)
+            if isnumeric(E.event.(restfields{f}))
+                E.event.(restfields{f})(ib) = [E.event.(restfields{f});zeros(Nnewfeat,1)];
+            else
+                E.event.(restfields{f})(ib) = [E.event.(restfields{f});repmat({''},Nnewfeat,1)];
+            end
+        end
+    end
 
-Nsamples = length(E.sdesc);
-E.dat = false(Nevents,Nsamples);
-for i = 1:Nevents
-    E.dat(i,:) = 0 ~= cols{i+1};
-end
+    %% read in rest of event.call file as cell array of columns
+
+    % make textscan format string
+    if hdrline(end) == char(10)
+        lf_char = '\n';%char(10);
+        pcunix_text = true;
+    else
+        lf_char = '\r';%char(13);
+        pcunix_text = false;
+    end
+
+
+    fmstr = ['%s',lin_fmt,repmat(' %d',1,Nevents),lf_char];
+    try
+        if pcunix_text
+            cols = textscan(fid,fmstr,'ReturnOnError',0,'Delimiter',dlm,'WhiteSpace','\r');
+        else
+            cols = textscan(fid,fmstr,'ReturnOnError',0,'Delimiter',dlm);
+        end
+        fclose(fid);
+    catch me
+        fclose(fid);
+        error(['event.call error: textscan error ''',me.message,'''']);
+    end
+
+    % test for short columns
+    if any(diff(cellfun(@length,cols)))
+        error('event.call error: missing event data');
+    end
+
+    % sample column
+    E.sdesc = strtrim(cols{1});
+    % optional lineage column
+    Nsamples = length(E.sdesc);
+    if have_lineage
+        lintext = strtrim(cols{2});
+        E.pcname = unique(lintext,'stable')';
+        E.pcx = cellfun(@(x) find(ismember(lintext,x)),E.pcname,'UniformOutput',false);
+    else
+        E.pcx = {};
+        E.pcname = {};
+    end
+
+    % data columns
+    E.dat = false(Nevents,Nsamples);
+    for i = 1:Nevents
+        E.dat(i,:) = 0 ~= cols{i+have_lineage+1};
+    end
+    E.dat = logical(E.dat);
 
 end % function
