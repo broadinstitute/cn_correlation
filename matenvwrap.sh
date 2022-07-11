f1baa2c8492d962cafa0b36a9884b54b66883526
stevenschumacher <schum@broadinstitute.org>
2022-07-11 18:11:03 -0400

    old LD_LIBRARY_PATH now appended after matlab library paths in permutation environment to override user matlab settings

--- matenvwrap.sh
+++ matenvwrap.sh
@@ -3,7 +3,8 @@
 export MLR=`cat $2/matlabroot`
 # add MCR paths to library search path
 echo setting MCR environment using matlabroot $MLR
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$MLR/bin/glnxa64:$MLR/sys/java/jre/glnxa64/jre/lib/amd64:$MLR/sys/java/jre/glnxa64/jre/lib/amd64/server:$MLR/sys/java/jre/glnxa64/jre/lib/amd64/jli:$MLR/sys/os/glnxa64:$MLR/runtime/glnxa64
+export LD_LIBRARY_PATH=$MLR/bin/glnxa64:$MLR/sys/java/jre/glnxa64/jre/lib/amd64:$MLR/sys/java/jre/glnxa64/jre/lib/amd64/server:$MLR/sys/java/jre/glnxa64/jre/lib/amd64/jli:$MLR/sys/os/glnxa64:$MLR/runtime/glnxa64:$LD_LIBRARY_PATH
+
 #run command line
 $*
 
